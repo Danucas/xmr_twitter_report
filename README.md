aa2593afb104dcaaf6ed988877beb01500edb908
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-06 00:58:18 +0000

    crontab scheduling at 8, 12 and 16

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# xmr_twitter_report
+# Xmr Twitter report
 Gives a daily report about monero exchanges to BTC and USD
+
+# Feel free to contribute
+XMR addr 
+`82Z6TuNkL2REwmJArtZjYJequKmsMSwa32TxxAVH9m4eVLL2BFugrrURiYT4kUm1kdPecaoKwrt5wZQjiVnfHxdnUNpne8z`
--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Setup the environment to run the python script
+# Scheduling with crontab at 8, 12, and 14
 sudo apt-get -y install python3-requests
 sudo apt-get -y install git
 git clone https://github.com/danrocus1994/xmr_twitter_report
