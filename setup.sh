0e05920055a633aa7c2f4a780e42d7f69afad123
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-06 01:33:03 +0000

    adding keys handler

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ else
 	echo "CONS_SEC="$2 >> $HOME/keys/sec_twitter_keys
 	echo "TOKE_KEY="$3 >> $HOME/keys/sec_twitter_keys
 	echo "TOKE_SEC="$4 >> $HOME/keys/sec_twitter_keys
-	#sudo apt-get -y install python3-requests
-	#sudo apt-get -y install git
-	#git clone https://github.com/danrocus1994/xmr_twitter_report
-	#echo '* 8,12,16 * * * '$HOME'/xmr_twitter_report/xmr_caller.py' | crontab -
+	sudo apt-get -y install python3-requests
+	sudo apt-get -y install git
+	git clone https://github.com/danrocus1994/xmr_twitter_report
+	echo '* 8,12,16 * * * '$HOME'/xmr_twitter_report/xmr_caller.py' | crontab -
 fi
